9a98390f10399e07759897c8c570adfdba0dbec7
faprayyy <fauzanjr1@gmail.com>
2021-05-07 13:37:54 +0700

    done instrument test with Handler delay

--- Academy/app/src/androidTest/java/com/dicoding/faprayyy/academy/ExampleInstrumentedTest.kt
+++ Academy/app/src/androidTest/java/com/dicoding/faprayyy/academy/ExampleInstrumentedTest.kt
@@ -1,12 +1,24 @@
 package com.dicoding.faprayyy.academy
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.rule.ActivityTestRule
+import com.dicoding.faprayyy.academy.ui.home.HomeActivity
+import com.dicoding.faprayyy.academy.utils.DataDummy
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +27,65 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    private val dummyCourse = DataDummy.generateDummyCourses()
+
+    @get:Rule
+    var activityRule = ActivityTestRule(HomeActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dicoding.faprayyy.academy", appContext.packageName)
+    fun loadCourses() {
+        delayTwoSecond()
+        onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
+    }
+
+    @Test
+    fun loadDetailCourse() {
+        delayTwoSecond()
+        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        delayTwoSecond()
+        onView(withId(R.id.text_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_title)).check(matches(withText(dummyCourse[0].title)))
+        onView(withId(R.id.text_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_date)).check(matches(withText("Deadline ${dummyCourse[0].deadline}")))
+    }
+
+    @Test
+    fun loadModule() {
+        delayTwoSecond()
+        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        delayTwoSecond()
+        onView(withId(R.id.btn_start)).perform(click())
+        delayTwoSecond()
+        onView(withId(R.id.rv_module)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadDetailModule() {
+        delayTwoSecond()
+        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        delayTwoSecond()
+        onView(withId(R.id.btn_start)).perform(click())
+        delayTwoSecond()
+        onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        delayTwoSecond()
+        onView(withId(R.id.web_view)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadBookmarks() {
+        onView(withText("Bookmark")).perform(click())
+        delayTwoSecond()
+        onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
+    }
+
+    private fun delayTwoSecond() {
+        try {
+            Thread.sleep(2000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 }
